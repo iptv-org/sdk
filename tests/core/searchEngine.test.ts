34cebed36d94c861f34d122fcd7af13dfe418417
freearhey <7253922+freearhey@users.noreply.github.com>
2025-11-25 10:58:41 +0300

    Update searchEngine.test.ts

--- tests/core/searchEngine.test.ts
+++ tests/core/searchEngine.test.ts
@@ -1,12 +1,9 @@
 import { SearchEngine } from '../../src/core/searchEngine'
 import { expect, test, describe } from 'vitest'
-import { Collection } from '@freearhey/core'
 
 describe('SearchEngine', () => {
   test('createIndex()', () => {
-    const searchIndex = SearchEngine.createIndex<{ id: number }>(
-      new Collection([{ id: 1 }, { id: 2 }])
-    )
+    const searchIndex = SearchEngine.createIndex<{ id: number }>([{ id: 1 }, { id: 2 }])
 
     expect(searchIndex.fields[0]).toBe('id')
   })
